f20c7fe407deff6e686d3c27760103d4179678a1
Hrishikesh Barhate <HrishikeshBarhate@gmail.com>
2022-05-17 11:23:48 +0530

    DSA- Rotation strings

--- src/main/java/com/dsa/DsaApplication.java
+++ src/main/java/com/dsa/DsaApplication.java
@@ -308,5 +308,22 @@ public class DsaApplication {
 		}
 		return charsList;
 	}
+	
+	/* Check if strings are rotations of each other or not
+	 * eg given s1 = ABCD and s2 = CDAB, return true, given s1 = ABCD, and s2 = ACBD , return false)
+	 * String str1 = "AACD"; String str2 = "ACDA";
+	 * 
+	 * if (areRotations(str1, str2))
+	 * System.out.println("Strings are rotations of each other"); 
+	 * else
+	 * System.out.printf("Strings are not rotations of each other");
+	 */
+	static boolean areRotations(String str1, String str2)
+    {
+        // There lengths must be same and str2 must be
+        // a substring of str1 concatenated with str1. 
+        return (str1.length() == str2.length()) &&
+               ((str1 + str1).indexOf(str2) != -1);
+    }
   
 }
