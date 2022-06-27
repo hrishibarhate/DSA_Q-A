f0842963745cba056e29a6a7f59929034722de7f
Hrishikesh Barhate <HrishikeshBarhate@gmail.com>
2022-06-27 12:38:56 +0530

    DSA - ReverseStringByWord

--- src/main/java/com/dsa/DsaApplication.java
+++ src/main/java/com/dsa/DsaApplication.java
@@ -19,9 +19,9 @@ public class DsaApplication {
 	private static void replaceFunctions() {
 		String x = "Company  \"Brillio\" ";
 
-        String removeAllOccurances = x.replaceAll("\"", "");
+		String removeAllOccurances = x.replaceAll("\"", "");
 
-        String addRoundBrackets = x.replaceFirst("\"", "{").replaceFirst("\"", "}");
+		String addRoundBrackets = x.replaceFirst("\"", "{").replaceFirst("\"", "}");
 
 		System.out.println(removeAllOccurances);
 		System.out.println(addRoundBrackets);
@@ -438,4 +438,25 @@ public class DsaApplication {
 		System.out.print("Reversed string : " + reversedString);
 	}
 
+//  Reverse the string word by word but each word’s characters remain unchanged.
+//  Example
+//  Original string : how to do in java
+//  Reversed string : java in do to how
+
+	public static void reverseWordByWord() {
+		String originalStr = "how to do in java";
+		String words[] = originalStr.split("\\s");
+		String reversedString = "";
+
+		// Reverse each word's position
+		for (int i = 0; i < words.length; i++) {
+			if (i == words.length - 1)
+				reversedString = words[i] + reversedString;
+			else
+				reversedString = " " + words[i] + reversedString;
+		}
+
+		// Displaying the string after reverse
+		System.out.print("Reversed string : " + reversedString);
+	}
 }
